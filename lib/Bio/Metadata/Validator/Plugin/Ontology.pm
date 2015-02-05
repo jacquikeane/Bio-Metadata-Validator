b6bb71ec92641ac99aa76a16d563159b891df08d
John Tate <jt6@sanger.ac.uk>
2015-02-05 11:22:23 +0000

    Add small ontology files for testing plugin
    Now has two configuration files, one for using the small hand-crafted
    ontology files that are in t/data, and one for use with the full
    ontologies that can be downloaded.

--- lib/Bio/Metadata/Validator/Plugin/Ontology.pm
+++ lib/Bio/Metadata/Validator/Plugin/Ontology.pm
@@ -56,4 +56,29 @@ __PACKAGE__->meta->make_immutable;
 
 1;
 
+__END__
+
+# Just out of curiosity, I tested using system grep to search the files, but it
+# turns out to be slower than the current method of caching the ontologies in
+# memory as a hash.
+#
+# Using the "validate" method above the wallclock time for the tests (with the
+# ontology files already cached on disk) was 4s. Using system grep the
+# wallclock time for the tests is 18s.
+
+sub validate {
+  my ( $self, $value, $field_definition ) = @_;
+
+  die 'ERROR: the Ontology validator requires a file path for the ontology'
+    unless defined $field_definition->{path};
+
+  my $ontology_file = $field_definition->{path};
+  die "ERROR: couldn't find ontology file '$ontology_file': $!"
+    unless -e $ontology_file;
+
+  my $grep = "grep '^id: ${value}\$' $ontology_file";
+  my $capture = qx/$grep/;
+
+  return $capture ? 1 : 0;
+}
 
